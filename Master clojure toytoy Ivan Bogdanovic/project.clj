765e9a84b8a6489542ef4c3eb127b52396d82cc9
Ivan Bogdanovic <43254872+pilulica@users.noreply.github.com>
2019-09-13 15:08:44 +0200

    Add, Update, Delete Dependencies
    Refactoring dependencies

--- Master clojure toytoy Ivan Bogdanovic/project.clj
+++ Master clojure toytoy Ivan Bogdanovic/project.clj
@@ -3,12 +3,14 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]
-                 [hiccup "1.0.5"]
+                 [hiccup "2.0.0-alpha1"]
                  [hiccup-table "0.2.0"]
+                 [hiccup-bootstrap "0.1.2"]
                  [ring-server "0.3.1"]
+                 [ring/ring-core "1.6.1"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [mysql/mysql-connector-java "5.1.18"]]
-  :plugins [[lein-ring "0.8.12"]]
+  :plugins [[lein-ring "0.9.7"]]
   :ring {:handler toytoy.handler/app}
   :profiles
   {:uberjar {:aot :all}
